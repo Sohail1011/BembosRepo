bdfcea91e947a52ea7935d0e045951e542b78245
Sohail Iran <aratavalga@gmail.com>
2023-11-04 12:36:52 -0500

    mission complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { RiShoppingCartFill } from 'react-icons/ri';
+import { MdLocalDining } from 'react-icons/md';
+
 export const Header = () => {
   return (
-    <div>Soy Jaime</div>
+    <article className='enlaces'>
+      <section className="zonas">
+        <ul>
+          <li><a href="">Menú</a></li>
+          <li><a href="">Beneficios</a></li>
+          <li><a href="">Promociones</a></li>
+          <li><a href="">Locales</a></li>
+        </ul>
+      </section>
+      <section className='companium'>
+        <button>
+          <MdLocalDining size={"2rem"} /> <div><p>Pide en tiendas</p><span>SIN COLAS</span></div>
+        </button>
+      </section>
+      <section className='companium'>
+        <button><RiShoppingCartFill size={"2rem"} /></button>
+      </section>
+    </article>
   )
 }
