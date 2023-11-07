98e86dab1259a1a9cee8d3686abba6b3a159a88e
Sohail Iran <aratavalga@gmail.com>
2023-11-06 21:30:53 -0500

    formatos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,41 @@ import '../styles/Footer.css'
 export const Footer = () => {
   return (
     <footer className="ultimate">
-      <div className='social-image'>
+      <section className='social-image'>
         <RiFacebookCircleFill size={"3rem"} />
         <TiSocialYoutubeCircular size={"3rem"} />
         <SiTwitter size={"3rem"} />
         <TiSocialInstagramCircular size={"3rem"} />
-      </div>
-      <BembosLogo />
+      </section>
+      <section className="metodos-pago">
+        <article className="locales">
+          <div className="part-1">
+            <a href="">Locales</a>
+            <a href="">Zonas de Reparto</a>
+            <a href="">Nosotros</a>
+            <a href="">Atención al Cliente</a>
+            <a href="">Preguntas Frecuentes</a>
+            <a href="">Mapa del sitio</a>
+          </div>
+          <div className="part-2">
+            <a href="">Políticas de Datos Personales</a>
+            <a href="">Términos y condiciones de</a>
+            <a href="">Promociones</a>
+            <a href="">Derechos ARCO</a>
+          </div>
+          <div className="part-3">
+            <BembosLogo />
+          </div>
+        </article>
+        <article className="visa">
+          <div className="modes">
+            <p>FORMAS DE PAGO</p>
+          </div>
+          <div className="mastercard"></div>
+          <div className="safety"></div>
+          <div className="document"></div>
+        </article>
+      </section>
     </footer>
   )
 }
