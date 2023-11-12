85c9e63be48eb163cc7874b8ab96b0546e7cdff9
Sohail Iran <aratavalga@gmail.com>
2023-11-12 16:54:42 -0500

    nav buttons

--- src/components/CardLocation.jsx
+++ src/components/CardLocation.jsx
@@ -3,10 +3,12 @@ import { GoLocation } from 'react-icons/go';
 export const CardLocation = () => {
     return (
         <div className="location">
-            <p>Se está mostrando el menú del <br /> restaurante</p>
             <div className="ubicacion">
-                <GoLocation size={"1.5rem"} />
-                <h4>GENERAL</h4>
+                <p>Se está mostrando el menú del <br /> restaurante</p>
+                <div className='globo'>
+                    <GoLocation size={"1.5rem"} />
+                    <h4>GENERAL</h4>
+                </div>
             </div>
             <div className="options">
                 <button>Cambiar</button>
