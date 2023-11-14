dde045001df66b6193f0ab02e9c8bb87111de6f3
Sohail Iran <aratavalga@gmail.com>
2023-11-14 10:09:47 -0500

    vasos menu

--- src/components/Vasos.jsx
+++ src/components/Vasos.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 export const Vasos = ({ informacion = 'null', picture }) => {
     return (
-        <div>
+        <div className="refrescos-vasos">
             <h3>{informacion}</h3>
             <img src={picture} alt={informacion} />
+            <button>Ver Todos</button>
         </div>
     )
 }
